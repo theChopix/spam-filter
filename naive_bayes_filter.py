a6c6f0300a94d48c62dc77db18c74e187a4f8ee7
theChopix <kratkyjan244@gmail.com>
2022-07-02 22:23:39 +0200

    adjustments

--- naive_bayes_filter.py
+++ naive_bayes_filter.py
@@ -1,11 +1,19 @@
 from utils import *
-
+import re
+from corpus import Corpus
 
 SPAM_INDEX = 0
 HAM_INDEX = 1
 
 
-class BayesianFilter:
+#words
+#IPs   (re.compile('[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'))
+#links (re.compile('http://.{,20}\..{,15}\.'))
+#upper (re.compile('[A-Z]{10,}'))
+#marks (re.compile('[<!*?#%>]{3,}'))
+
+
+class NaiveBayesFilter:
 
     def __init__(self, pattern):
         self.pattern = pattern
@@ -73,7 +81,7 @@ class BayesianFilter:
             self.train(my_truth_dict, corpus)
             return self.classify(email_body)
 
-
+#
 
 
 
